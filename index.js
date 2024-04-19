daec811ea5932af9fcd0b097d403acf07739b2a5
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-19 08:46:34 -0300

    start server with express

---
+++ index.js
@@ -0,0 +1,23 @@
+import { fileURLToPath } from "url";
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import cors from "cors";
+import "dotenv/config"
+
+const app = express();
+
+app.set("port", process.env.PORT || 2504);
+app.listen(app.get("port"), () => {
+  console.log(`I'm in Port ${app.get("port")}`);
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/public")));
