d52773568075270d086be774167557c4f7f82bb7
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-01 10:37:43 -0300

    se creó el helper para enviar los emails a la hora de enviar correos para confirmar cuenta

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import { sendEmailForgorPassword } from "../helpers/generatedEmail.js";
 import generatedJWT from "../helpers/generatedJWT.js";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
